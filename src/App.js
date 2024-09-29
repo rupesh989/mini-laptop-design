72b2e62fdb1ac74e4cb29cb6fe99ae27a282dfd2
rupesh989 <rupeshbavaskar24@gmail.com>
2024-09-29 23:37:45 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Background from "./components/Background";
+import Laptop from "./components/Laptop";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Background />
+      <Laptop />
+    </>
   );
-}
+};
 
 export default App;
